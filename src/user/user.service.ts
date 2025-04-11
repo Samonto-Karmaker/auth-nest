f94abc9881124a971a080fd7212aebd05bec4b21
Samonto Karmaker <samonto0726@gmail.com>
2025-04-11 13:47:24 +0600

    feat: added registerUser service with hashed password feature

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,22 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable } from "@nestjs/common";
+import { registerUserDto } from "./dtos/registerUser.dto";
+import * as bcrypt from "bcrypt";
 
 @Injectable()
-export class UserService {}
+export class UserService {
+    // This is a placeholder for the actual user data source
+    private users: registerUserDto[] = [];
+
+    // Method to register a new user
+    async registerUser(userData: registerUserDto) {
+        const newUser = { ...userData };
+        newUser.password = await bcrypt.hash(userData.password, 10); // Hash the password
+        this.users.push(newUser);
+        return newUser;
+    }
+
+    // Method to get all users
+    getAllUsers() {
+        return this.users;
+    }
+}
