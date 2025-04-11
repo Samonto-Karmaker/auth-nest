553904e7f38cceac1955e0d43b5a8ccda12c3568
Samonto Karmaker <samonto0726@gmail.com>
2025-04-11 14:55:02 +0600

    feat: rename User interface to UserInterface and create User entity implementing it

---
+++ src/user/user.entity.ts
@@ -0,0 +1,30 @@
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import { UserInterface, UserRole } from "./interfaces/user.interface";
+
+@Entity({ name: "users" })
+export class User implements UserInterface {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column({ type: "varchar" })
+    username: string;
+
+    @Column({ type: "varchar", unique: true })
+    email: string;
+
+    @Column({ type: "varchar" })
+    password: string;
+
+    @Column({ type: "enum", enum: UserRole, default: UserRole.USER })
+    role: UserRole;
+
+    @Column({ type: "timestamp", default: () => "CURRENT_TIMESTAMP" })
+    createdAt: Date;
+
+    @Column({
+        type: "timestamp",
+        default: () => "CURRENT_TIMESTAMP",
+        onUpdate: "CURRENT_TIMESTAMP",
+    })
+    updatedAt: Date;
+}
