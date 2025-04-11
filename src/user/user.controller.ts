7d21f1178b152e435794a06219355dae7f134640
Samonto Karmaker <samonto0726@gmail.com>
2025-04-11 15:39:00 +0600

    feat: user registration in db complete

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,15 +1,10 @@
-import { Body, Controller, Get, Post } from "@nestjs/common";
+import { Body, Controller, Post } from "@nestjs/common";
 import { UserService } from "./user.service";
 import { registerUserDto } from "./dto/registerUser.dto";
 
 @Controller("user")
 export class UserController {
     constructor(private readonly userService: UserService) {}
-
-    @Get()
-    getAllUsers() {
-        return this.userService.getAllUsers();
-    }
 
     @Post("/register")
     registerUser(@Body() userData: registerUserDto) {
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,22 +1,41 @@
 import { Injectable } from "@nestjs/common";
 import { registerUserDto } from "./dto/registerUser.dto";
 import * as bcrypt from "bcrypt";
+import { InjectRepository } from "@nestjs/typeorm";
+import { User } from "./user.entity";
+import { Repository } from "typeorm";
 
 @Injectable()
 export class UserService {
-    // This is a placeholder for the actual user data source
-    private users: registerUserDto[] = [];
+    constructor(
+        @InjectRepository(User)
+        private readonly userRepository: Repository<User>,
+    ) {}
 
-    // Method to register a new user
-    async registerUser(userData: registerUserDto) {
-        const newUser = { ...userData };
-        newUser.password = await bcrypt.hash(userData.password, 10); // Hash the password
-        this.users.push(newUser);
-        return newUser;
-    }
+    async registerUser(userData: registerUserDto): Promise<User> {
+        try {
+            // Check if the user already exists
+            const existingUser = await this.userRepository.findOne({
+                where: { email: userData.email },
+                select: ["id", "email"],
+            });
 
-    // Method to get all users
-    getAllUsers() {
-        return this.users;
+            if (existingUser) {
+                throw new Error("User already exists");
+            }
+
+            const hashedPassword = await bcrypt.hash(userData.password, 10);
+            const newUser = this.userRepository.create({
+                ...userData,
+                password: hashedPassword,
+            });
+            return await this.userRepository.save(newUser);
+        } catch (error) {
+            console.error("Error registering user:", error);
+            if (error instanceof Error) {
+                throw new Error(error.message);
+            }
+            throw new Error("Error registering user");
+        }
     }
 }
