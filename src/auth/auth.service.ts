6e907d81db3dc2c4c540d60262fb229984268eeb
Samonto Karmaker <samonto0726@gmail.com>
2025-04-11 16:58:54 +0600

    feat: implement user validation and login methods in AuthService

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,57 @@
-import { Injectable } from "@nestjs/common";
+import {
+    Injectable,
+    NotFoundException,
+    UnauthorizedException,
+} from "@nestjs/common";
+import { JwtService } from "@nestjs/jwt";
+import { User } from "src/user/user.entity";
+import { UserService } from "src/user/user.service";
+import { LoginDto } from "./dto/login.dto";
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+    constructor(
+        private readonly userService: UserService,
+        private readonly jwtService: JwtService,
+    ) {}
+
+    async validateUser(email: string, password: string): Promise<User> {
+        try {
+            const user = await this.userService.getUserByEmail(email);
+            if (!user) {
+                throw new NotFoundException("User not found");
+            }
+
+            const isPasswordValid = await user.validatePassword(password);
+            if (!isPasswordValid) {
+                throw new UnauthorizedException("Invalid credentials");
+            }
+
+            return user;
+        } catch (error) {
+            console.error("Error validating user:", error);
+            if (
+                error instanceof NotFoundException ||
+                error instanceof UnauthorizedException
+            ) {
+                throw error;
+            }
+            throw new UnauthorizedException("Invalid credentials");
+        }
+    }
+
+    async login(loginDto: LoginDto) {
+        const user = await this.validateUser(loginDto.email, loginDto.password);
+        const payload = { sub: user.id, role: user.role };
+        const accessToken = await this.jwtService.signAsync(payload);
+        return {
+            accessToken,
+            user: {
+                id: user.id,
+                username: user.username,
+                email: user.email,
+                role: user.role,
+            },
+        };
+    }
+}
