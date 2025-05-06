6601f7c183771f713886a110a78dcc9314674636
Samonto Karmaker <samonto0726@gmail.com>
2025-05-06 18:39:18 +0600

    fix: set synchronize option to true for TypeORM configuration

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -15,5 +15,5 @@ export const typeOrmConfig: TypeOrmModuleOptions = {
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     entities: [path.resolve(__dirname, "..", "**", "*.entity.{ts,js}")],
-    synchronize: process.env.NODE_ENV === "development",
+    synchronize: true,
 };
