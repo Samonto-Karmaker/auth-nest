3fca10aa86740c2cf0b65f5cea304d6a7d0acc38
Samonto Karmaker <samonto0726@gmail.com>
2025-05-07 11:42:45 +0600

    feat: add ApiProperty decorators in DTOs for improved Swagger documentation

--- src/auth/dto/login.dto.ts
+++ src/auth/dto/login.dto.ts
@@ -1,11 +1,25 @@
-/* eslint-disable @typescript-eslint/no-unsafe-call */
+import { ApiProperty } from "@nestjs/swagger";
 import { IsEmail, IsNotEmpty, IsString } from "class-validator";
 
 export class LoginDto {
+    @ApiProperty({
+        description: "Email of the user",
+        example: "john.doe@example.com",
+        required: true,
+        format: "email",
+        type: String,
+    })
     @IsEmail()
     @IsNotEmpty()
     email: string;
 
+    @ApiProperty({
+        description: "Password of the user",
+        example: "password123",
+        required: true,
+        format: "password",
+        type: String,
+    })
     @IsNotEmpty()
     @IsString()
     password: string;
--- src/user/dto/registerUser.dto.ts
+++ src/user/dto/registerUser.dto.ts
@@ -1,4 +1,4 @@
-/* eslint-disable @typescript-eslint/no-unsafe-call */
+import { ApiProperty } from "@nestjs/swagger";
 import { UserRole } from "../interface/user.interface";
 import {
     IsEmail,
@@ -9,19 +9,47 @@ import {
 } from "class-validator";
 
 export class registerUserDto {
+    @ApiProperty({
+        description: "Username of the user",
+        example: "john_doe",
+        required: true,
+        type: String,
+    })
     @IsString()
     @IsNotEmpty()
     username: string;
 
+    @ApiProperty({
+        description: "Email of the user",
+        example: "john.doe@example.com",
+        required: true,
+        format: "email",
+        type: String,
+    })
     @IsEmail()
     @IsNotEmpty()
     email: string;
 
+    @ApiProperty({
+        description: "Password of the user",
+        example: "password123",
+        required: true,
+        format: "password",
+        minLength: 6,
+        type: String,
+    })
     @IsString()
     @IsNotEmpty()
     @MinLength(6)
     password: string;
 
+    @ApiProperty({
+        description: "Role of the user",
+        example: UserRole.USER,
+        required: true,
+        enum: UserRole,
+        type: String,
+    })
     @IsEnum(UserRole)
     @IsNotEmpty()
     role: UserRole;
