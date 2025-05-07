14d97185662250e4fb8dc165983b587fde74273d
Samonto Karmaker <samonto0726@gmail.com>
2025-05-07 11:26:08 +0600

    feat: add Swagger documentation setup for AuthNest API

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,24 @@ import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import { ValidationPipe } from "@nestjs/common";
 import { config } from "dotenv";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 
 config();
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
     app.useGlobalPipes(new ValidationPipe());
+
+    const swaggerConfig = new DocumentBuilder()
+        .setTitle("AuthNest API")
+        .setDescription("API documentation for AuthNest")
+        .setVersion("1.0")
+        .addBearerAuth()
+        .build();
+
+    const document = SwaggerModule.createDocument(app, swaggerConfig);
+    SwaggerModule.setup("docs", app, document);
+
     await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
